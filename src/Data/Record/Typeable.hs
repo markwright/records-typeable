691877dff520944d6213e456db67218c4f46051c
Mark Wright <markwright@internode.on.net>
2011-05-31 22:05:41 +1000

    Initial version

---
+++ src/Data/Record/Typeable.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, ScopedTypeVariables, TypeOperators #-}
+
+module Data.Record.Typeable where
+
+import Data.TypeFun
+import Data.Record
+import Data.Typeable
+
+-- | Empty record scheme Typeable instance.
+instance Typeable style => 
+  Typeable (X style) where
+    typeOf (_::X style) = mkTyConApp (mkTyCon "Data.Record.X") [ typeOf (undefined::style) ]
+
+-- | Non-empty record schemes Typeable instances.
+instance (Typeable (rec style), Typeable name, Typeable (App style sort)) => 
+  Typeable ((rec :& name ::: sort) style) where
+    typeOf (rec :& field) = mkTyConApp (mkTyCon "Data.Record.:&") [ typeOf rec, typeOf field ]
+
+-- | Record fields Typeable instances.
+instance (Typeable name, Typeable (App style sort)) =>
+  Typeable ((name ::: sort) style) where
+    typeOf (name := val) = mkTyConApp (mkTyCon "Data.Record.:=") [ typeOf name, typeOf val ]

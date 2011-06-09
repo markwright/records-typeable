42d8c1df3a4aa00951f8b33eff2fdeefb57b6dc1
Mark Wright <markwright@internode.on.net>
2011-06-09 21:17:28 +1000

    Add README, haddock doc for Eq

--- src/Data/Record/Eq.hs
+++ src/Data/Record/Eq.hs
@@ -7,16 +7,21 @@ import Data.TypeFun
 import Data.Record
 import Data.Typeable
 
+-- | Eq instance for KindStar
 deriving instance Eq KindStar
 
+-- | Eq instance for Id KindStar
 deriving instance Eq KindStar =>
   Eq (Id KindStar)
 
+-- | Eq instance for X style
 deriving instance Eq style =>
   Eq (X style)
 
+-- | Eq instance for :&
 deriving instance (Eq (rec style), Eq name, Eq (App style sort)) => 
   Eq ((rec :& name ::: sort) style)
 
+-- | Eq instance for name
 deriving instance (Eq name, Eq (App style sort)) =>
   Eq ((name ::: sort) style)
